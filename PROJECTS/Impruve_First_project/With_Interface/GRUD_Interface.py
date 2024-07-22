caf905ff3309a01c5bb0b7459a86c3d53533d373
Mahmud Afandiyev <efendiyev.mahmud@gmail.com>
2024-07-22 15:17:59 +0400

    create simple GU without interaction

--- PROJECTS/Impruve_First_project/With_Interface/GRUD_Interface.py
+++ PROJECTS/Impruve_First_project/With_Interface/GRUD_Interface.py
@@ -2,3 +2,12 @@ from PROJECTS.Impruve_First_project.With_Interface.Functions import get_dat, get
 import time
 import os
 
+import FreeSimpleGUI as sg
+
+lable = sg.Text("Input data")
+input_box = sg.InputText("")
+add_btn = sg.Button("ADD")
+
+windows = sg.Window('CRUD Projects', [[lable], [input_box, add_btn]])
+windows.read()
+windows.close()
