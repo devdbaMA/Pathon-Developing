57805169bfb0d1743a2007e870b7f4e051ed0f73
Mahmud Afandiyev <efendiyev.mahmud@gmail.com>
2024-07-23 16:43:59 +0400

    arch project 23072024 version

--- PROJECTS/Archve_with_GU/Arch_GU_use.py
+++ PROJECTS/Archve_with_GU/Arch_GU_use.py
@@ -5,10 +5,28 @@ label2 = sg.Text("Select destination folder")
 in_1 = sg.InputText("")
 in_2 = sg.InputText("")
 
-btn1 = sg.FilesBrowse("Choose")
-btn2 = sg.FolderBrowse("Choose")
+btn1 = sg.FilesBrowse("Choose", key="files")
+btn2 = sg.FolderBrowse("Choose", key="folder")
 btn3 = sg.Button("Compress")
 
-window = sg.Window("FILE COMPRESSER", [[label1,in_1,btn1], [label2,in_2,btn2],[btn3]])
-window.read()
+layout = [
+    [label1, in_1, btn1],
+    [label2, in_2, btn2],
+    [btn3]
+]
+
+window = sg.Window("FILE COMPRESSER", layout)
+
+while True:
+    event, value = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == "Compress":
+        filepath = value['files'].split(';')
+        folder = value['folder']
+        print("Files to compress:", filepath)
+        print("Destination folder:", folder)
+
 window.close()
