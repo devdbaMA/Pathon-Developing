9bad6752850b73a776905bb7319a4566e9ae4cda
Mahmud Afandiyev <efendiyev.mahmud@gmail.com>
2024-07-24 11:13:17 +0400

    Archived_programs 24072024

--- PROJECTS/Archve_with_GU/Arch_GU_use.py
+++ PROJECTS/Archve_with_GU/Arch_GU_use.py
@@ -1,18 +1,24 @@
 import FreeSimpleGUI as sg
+import zipfile
+import os
 
 label1 = sg.Text("Select files to compress")
 label2 = sg.Text("Select destination folder")
-in_1 = sg.InputText("")
-in_2 = sg.InputText("")
+label3 = sg.Text("Enter name for compressed file (without extension)")
 
-btn1 = sg.FilesBrowse("Choose", key="files")
-btn2 = sg.FolderBrowse("Choose", key="folder")
-btn3 = sg.Button("Compress")
+in_files = sg.InputText("")
+in_folder = sg.InputText("")
+in_filename = sg.InputText("", size=(32, 1))
+
+btn_files = sg.FilesBrowse("Choose", key="files")
+btn_folder = sg.FolderBrowse("Choose", key="folder")
+btn_compress = sg.Button("Compress")
 
 layout = [
-    [label1, in_1, btn1],
-    [label2, in_2, btn2],
-    [btn3]
+    [label1, in_files, btn_files],
+    [label2, in_folder, btn_folder],
+    [label3, in_filename],
+    [btn_compress]
 ]
 
 window = sg.Window("FILE COMPRESSER", layout)
@@ -24,9 +30,20 @@ while True:
         break
 
     if event == "Compress":
-        filepath = value['files'].split(';')
+        filepaths = value['files'].split(';')
         folder = value['folder']
-        print("Files to compress:", filepath)
-        print("Destination folder:", folder)
+        zip_filename = value[2].strip()  # Input text index for filename is 2
+
+        if not filepaths or not folder or not zip_filename:
+            sg.popup("Please select files, a destination folder, and enter a file name.")
+            continue
+
+        zip_filepath = os.path.join(folder, f"{zip_filename}.zip")
+
+        with zipfile.ZipFile(zip_filepath, 'w') as zipf:
+            for filepath in filepaths:
+                zipf.write(filepath, os.path.basename(filepath))
+
+        sg.popup("Files compressed successfully!")
 
 window.close()
