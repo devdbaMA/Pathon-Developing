ae0ecdc5f92d577427fcd04d9b6236b3f81f3bb4
Mahmud Afandiyev <efendiyev.mahmud@gmail.com>
2024-07-30 15:32:13 +0400

    commit web app with insert and delete functions

--- PROJECTS/Impruve_First_project/Web_app/WEB_Interface.py
+++ PROJECTS/Impruve_First_project/Web_app/WEB_Interface.py
@@ -1,13 +1,26 @@
 import streamlit as st
 from Functions import get_dat_n, put_dat
 
-st.title("CRUD WEB Application")
-st.write("Data lists.")
+get_data = get_dat_n()
+
+
+def ins_data():
+    new_data = st.session_state["new_data"] + "\n"
+    get_data.append(new_data)
+    put_dat(get_data)
+    st.session_state["new_data"] = ""  # Clear the input field after adding
+
+
+st.title("CRUD Web Application")
+st.write("Data lists:")
 st.write("Data from text file")
 
-get_data = get_dat_n()
-for dat in get_data:
-    st.checkbox(dat)
+for index, dat in enumerate(get_data):
+    checkbox = st.checkbox(dat, key=dat)
+    if checkbox:
+        get_data.pop(index)
+        put_dat(get_data)
+        del st.session_state[dat]  # Corrected line to delete the session state entry
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Enter new data")
-st.button(label="ADD")
+st.text_input(label="", placeholder="Enter new data", on_change=ins_data, key="new_data")
