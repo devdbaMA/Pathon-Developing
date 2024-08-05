178dedbf6732208e6a5fc5a515beef5623290057
Mahmud Afandiyev <efendiyev.mahmud@gmail.com>
2024-08-05 12:22:04 +0400

    commit for Public

---
+++ Web_Public/Web_app/Functions.py
@@ -0,0 +1,12 @@
+file_path = 'D:\\My_DOC\\data_text\\list_info.txt'
+
+def get_dat_n():
+    """Read data in text file and print with indexes"""
+    with open(file_path, 'r') as file_in:
+        dat = file_in.readlines()
+    return dat
+
+def put_dat(dat):
+    """Write data in text file"""
+    with open(file_path, "w") as file:
+        file.writelines(dat)
